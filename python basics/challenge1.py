d88279836a03919de11cc3715a819a176d49fa1a
njorogeaugustine828@gmail.com <njorogeaugustine828@gmail.com>
2026-02-11 09:25:10 +0300

    first commit

---
+++ python basics/challenge1.py
@@ -0,0 +1,9 @@
+University = "Strathmore"
+Grade = "A"
+Favourite_course= "Law"
+Cluster_points ="44"
+
+print(f"My favourite University is {University} University")
+print(f"I attained grade {Grade}")
+print(f"Favourite course is {Favourite_course}")
+print(f"I have {Cluster_points} cluster points")
---
+++ python basics/hello.py
@@ -0,0 +1,14 @@
+# This is a comment
+print("hello from Augustine")
+# city / town
+print("I am from Thika")
+# my hobbies
+print("I like to play football,watch movies,dancing and Ice skating")
+
+age= 25
+height= 1.81
+weight=75
+
+print(f"I am {age} years old")
+print(f"I am also {height} metres tall")
+print(f"and weigh{weight} kgs")
