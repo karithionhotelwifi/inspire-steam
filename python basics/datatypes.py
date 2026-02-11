e6e3f22ab9b2edf48f6282f8d79aaf5be67390d7
njorogeaugustine828@gmail.com <njorogeaugustine828@gmail.com>
2026-02-11 09:52:52 +0300

    program to add two numbers

---
+++ python basics/datatypes.py
@@ -0,0 +1,11 @@
+# Nama : Augustine Mwangi
+# Date : 11/02/2026
+# Program to add two numbers
+
+first_number = 12
+second_number =86
+
+sum_number = first_number + second_number
+
+
+print(sum_numbers)
